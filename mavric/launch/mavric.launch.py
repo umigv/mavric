1dd066d1c508ac9a800a33709e1ae2bc0a8af112
Krishna <kjdih2@gmail.com>
2024-03-17 19:16:26 -0400

    fix packages

--- mavric/launch/mavric.launch.py
+++ mavric/launch/mavric.launch.py
@@ -12,68 +12,77 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
 
-   bno055_params = os.path.join(
-        get_package_share_directory('bno055'),
-        'config',
-        'bno055_params.yaml'
-        )
-   bno055_driver_node = launch_ros.actions.Node(
-        package = 'bno055',
-        executable = 'bno055',
-        parameters = [bno055_params]
+    bno055_params = os.path.join(
+        get_package_share_directory("bno055"), "config", "bno055_params.yaml"
     )
-   bno055 = LaunchDescription([bno055_driver_node,
-                                     RegisterEventHandler(
-                                         event_handler=OnProcessExit(
-                                             target_action=bno055_driver_node,
-                                             on_exit=[EmitEvent(
-                                                 event=Shutdown())],
-                                         )),
-                                     ])
-   
-   
-   velodyne_params = os.path.join(
-        get_package_share_directory('velodyne_driver'), 'config',
-        'VLP16-velodyne_driver_node-params.yaml')
-   
-   velodyne_driver_node = launch_ros.actions.Node(package='velodyne_driver',
-                                                   executable='velodyne_driver_node',
-                                                   output='both',
-                                                   parameters=[velodyne_params])
-   
-   velodyne = LaunchDescription([velodyne_driver_node,
-                                     RegisterEventHandler(
-                                         event_handler=OnProcessExit(
-                                             target_action=velodyne_driver_node,
-                                             on_exit=[EmitEvent(
-                                                 event=Shutdown())],
-                                         )),
-                                     ])
-   
-   robot_localization = LaunchDescription([
-        launch_ros.actions.Node(
-            package='robot_localization',
-            executable='ekf_node',
-            name='ekf_filter_node',
-            output='screen',
-            parameters=[os.path.join(get_package_share_directory("mavric"), 'config', 'mavric_ekf.yaml')],
-           ),
-        ])
-   
-   
-   
-   slam_toolbox = IncludeLaunchDescription(
-      PythonLaunchDescriptionSource([os.path.join(
-         get_package_share_directory('slam_toolbox'), 'launch'),
-         'online_async_launch.py'])
-      )
+    bno055_driver_node = launch_ros.actions.Node(
+        package="bno055", executable="bno055", parameters=[bno055_params]
+    )
+    bno055 = LaunchDescription(
+        [
+            bno055_driver_node,
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=bno055_driver_node,
+                    on_exit=[EmitEvent(event=Shutdown())],
+                )
+            ),
+        ]
+    )
 
+    velodyne_params = os.path.join(
+        get_package_share_directory("velodyne_driver"),
+        "config",
+        "VLP16-velodyne_driver_node-params.yaml",
+    )
 
-   
+    velodyne_driver_node = launch_ros.actions.Node(
+        package="velodyne_driver",
+        executable="velodyne_driver_node",
+        output="both",
+        parameters=[velodyne_params],
+    )
 
-   return LaunchDescription([
-      bno055,
-      velodyne,
-      robot_localization,
-      slam_toolbox
-   ])
+    velodyne = LaunchDescription(
+        [
+            velodyne_driver_node,
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=velodyne_driver_node,
+                    on_exit=[EmitEvent(event=Shutdown())],
+                )
+            ),
+        ]
+    )
+
+    robot_localization = LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="robot_localization",
+                executable="ekf_node",
+                name="ekf_filter_node",
+                output="screen",
+                parameters=[
+                    os.path.join(
+                        get_package_share_directory("mavric"),
+                        "config",
+                        "mavric_ekf.yaml",
+                    )
+                ],
+            ),
+        ]
+    )
+
+    slam_toolbox = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("slam_toolbox"),
+                    "launch",
+                    "online_async_launch.py",
+                )
+            ]
+        )
+    )
+
+    return LaunchDescription([bno055, velodyne, robot_localization, slam_toolbox])
